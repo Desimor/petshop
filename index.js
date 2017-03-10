0fd817c3ad8f6989e4293efd1e59913f84a09d24
Matthew Morais <moraismatthew@yahoo.com>
2017-03-09 19:25:25 -0500

    Implemented a server.get /animals to send json messages with the object when a request is made on the localhost.

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./secrets').mongoURI;
 
 //Connect tothe Database
-// mongoose.connect(mongoURI);
+mongoose.connect(mongoURI);
 // //Create the Mongoose Schema
-// var animalSchema = mongoose.Schema({
-//     color: String,
-//     size: String,
-//     type: String,
-//     price: Number
-// });
+var animalSchema = mongoose.Schema({
+    color: String,
+    size: String,
+    type: String,
+    price: Number
+});
 // //Create the Mongoose Model
-// var Animal = mongoose.model('Animal', animalSchema);
+var Animal = mongoose.model('Animal', animalSchema);
 // //Testing database stuff
 // var donkey = new Animal({
 //     color: 'gray',
@@ -23,15 +23,29 @@ var mongoURI = process.env.MONGOURI || require('./secrets').mongoURI;
 //     type: 'donkey',
 //     price: 180
 // });
-donkey.save(function(err, data){
-    if(err){
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
+// donkey.save(function(err, data){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(data);
+//     }
+// });
 //GET /animals
+server.get('/animals', function(req, res){
+    Animal.find({}, function(err, documents){
+        if(err){
+            res.status(500).json({
+                msg: err
+            });
+        } else {
+            res.status(200).json({
+                animals: documents
+            });
+        }
+    });
+});
 //GET /animals/:id
+server.get('/animals/:id', function(req, res){});
 //POST /animals
 //PUT /animals/:id
 //DELETE /animals/:id
